21fd0f169ee364f36012ab6aaae22822c3dcb27a
Deepak Maurya <dmaurya768@gmail.com>
2020-04-07 13:18:55 +0530

    custom calender

--- src/components/Finance.js
+++ src/components/Finance.js
@@ -1,25 +1,9 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
+import Calender from "./Calender";
 
-class Finance extends Component{
-    state={
-        arr:['5','10','20']
-    }
-    change = ()=>{
-        const newarr = [...this.state.arr];
-        newarr.push("100000");
-        this.setState({
-            arr:newarr
-        })
-        console.log(this.state.arr)
-    }
-    render(){
-        var classnames = "Finance-container container";
-        return(
-            <div className={classnames}>
-                <h1>length = {this.state.arr.length} element=  {this.state.arr.join(" ")}</h1>
-                <button onClick={this.change}>click me</button>
-            </div>
-        );
-    }
+class Finance extends Component {
+  render() {
+    return <Calender />;
+  }
 }
 export default Finance;
