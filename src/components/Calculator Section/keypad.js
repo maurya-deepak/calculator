abe1cadd856caf01d6941b060173315991ce2bc9
Deepak Maurya <30929093+deepakmauryamd@users.noreply.github.com>
2020-06-23 20:41:51 +0530

    Style change (#10)
    * changes..
    
    * bug removed
    
    * change.
    
    * change.
    
    * trying pwa
    
    * trying pwa
    
    * returned updated state in case of negative in temperature.
    
    * change
    
    * style changes

--- src/components/Calculator Section/keypad.js
+++ src/components/Calculator Section/keypad.js
@@ -21,186 +21,198 @@ const Keypad = (props) => {
   const checkClasses = `equal-key checkmark`;
   return (
     <div className="keypad-container">
-      <input
-        name="Ac"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="AC"
-        className={allclearClass}
-        disabled={isSpanActive || isOperatorActive}
-        title="All clear"
-      />
-      <button
-        aria-label="backspace"
-        id="icon"
-        name="backspace"
-        onClick={(e) => props.onClick(e.currentTarget.name)}
-        className={backspaceClass}
-        disabled={isOperatorActive}
-        title="Backspace"
-      >
-        <FontAwesomeIcon icon={faBackspace} />
-      </button>
-
-      <button
-        aria-label="mod"
-        id="icon"
-        name="%"
-        onClick={(e) => props.onClick(e.currentTarget.name)}
-        className={OperatorClasses}
-        disabled={isSpanActive}
-        title="Mode"
-      >
-        <FontAwesomeIcon icon={faPercent} />
-      </button>
-      <button
-        aria-label="divide"
-        id="icon"
-        name="/"
-        onClick={(e) => props.onClick(e.currentTarget.name)}
-        className={OperatorClasses}
-        disabled={isSpanActive}
-        title="Divide"
-      >
-        <FontAwesomeIcon icon={faDivide} />
-      </button>
-
-      <input
-        name="7"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="7"
-        disabled={isOperatorActive}
-      />
-      <input
-        name="8"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="8"
-        disabled={isOperatorActive}
-      />
-      <input
-        name="9"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="9"
-        disabled={isOperatorActive}
-      />
-      <button
-        aria-label="multiply"
-        id="icon"
-        name="*"
-        onClick={(e) => props.onClick(e.currentTarget.name)}
-        className={OperatorClasses}
-        disabled={isSpanActive}
-        title="Multiply"
-      >
-        <FontAwesomeIcon icon={faTimes} />
-      </button>
-
-      <input
-        name="4"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="4"
-        disabled={isOperatorActive}
-      />
-      <input
-        name="5"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="5"
-        disabled={isOperatorActive}
-      />
-      <input
-        name="6"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="6"
-        disabled={isOperatorActive}
-      />
-      <button
-        aria-label="minus"
-        id="icon"
-        name="-"
-        onClick={(e) => props.onClick(e.currentTarget.name)}
-        className={OperatorClasses}
-        disabled={isSpanActive}
-        title="Minus"
-      >
-        <FontAwesomeIcon icon={faMinus} />
-      </button>
-
-      <input
-        name="1"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="1"
-        disabled={isOperatorActive}
-      />
-      <input
-        name="2"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="2"
-        disabled={isOperatorActive}
-      />
-      <input
-        name="3"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="3"
-        disabled={isOperatorActive}
-      />
-      <button
-        aria-label="plus"
-        id="icon"
-        name="+"
-        onClick={(e) => props.onClick(e.currentTarget.name)}
-        className={OperatorClasses}
-        disabled={isSpanActive}
-        title="Plus"
-      >
-        <FontAwesomeIcon icon={faPlus} />
-      </button>
-      <input
-        name="0"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="0"
-        disabled={isOperatorActive}
-      />
-      <button
-        aria-label="dot"
-        id="icon"
-        name="."
-        className="dot"
-        disabled={isOperatorActive}
-        title="Dot"
-        onClick={(e) => props.onClick(e.currentTarget.name)}
-      >
-        <FontAwesomeIcon icon={faCircle} />
-      </button>
-      {isSpanActive || isOperatorActive ? (
-        <button
-          aria-label="Ok"
-          type="button"
-          className={checkClasses}
-          onClick={props.toggleisActiveSpan}
-          title="Ok"
-        >
-          <FontAwesomeIcon icon={faCheck} />
-        </button>
-      ) : (
+      <div className="flex-row">
         <input
-          name="="
+          name="Ac"
           onClick={(e) => props.onClick(e.target.name)}
           type="button"
-          value="="
-          className="equal-key"
-          title="Equal"
+          value="AC"
+          className={allclearClass}
+          disabled={isSpanActive || isOperatorActive}
+          title="All clear"
         />
-      )}
+        <button
+          aria-label="backspace"
+          id="icon"
+          name="backspace"
+          onClick={(e) => props.onClick(e.currentTarget.name)}
+          className={backspaceClass}
+          disabled={isOperatorActive}
+          title="Backspace"
+        >
+          <FontAwesomeIcon icon={faBackspace} />
+        </button>
+
+        <button
+          aria-label="mod"
+          id="icon"
+          name="%"
+          onClick={(e) => props.onClick(e.currentTarget.name)}
+          className={OperatorClasses}
+          disabled={isSpanActive}
+          title="Mode"
+        >
+          <FontAwesomeIcon icon={faPercent} />
+        </button>
+        <button
+          aria-label="divide"
+          id="icon"
+          name="/"
+          onClick={(e) => props.onClick(e.currentTarget.name)}
+          className={OperatorClasses}
+          disabled={isSpanActive}
+          title="Divide"
+        >
+          <FontAwesomeIcon icon={faDivide} />
+        </button>
+      </div>
+
+      <div className="flex-row">
+        <input
+          name="7"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="7"
+          disabled={isOperatorActive}
+        />
+        <input
+          name="8"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="8"
+          disabled={isOperatorActive}
+        />
+        <input
+          name="9"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="9"
+          disabled={isOperatorActive}
+        />
+        <button
+          aria-label="multiply"
+          id="icon"
+          name="*"
+          onClick={(e) => props.onClick(e.currentTarget.name)}
+          className={OperatorClasses}
+          disabled={isSpanActive}
+          title="Multiply"
+        >
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
+      </div>
+
+      <div className="flex-row">
+        <input
+          name="4"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="4"
+          disabled={isOperatorActive}
+        />
+        <input
+          name="5"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="5"
+          disabled={isOperatorActive}
+        />
+        <input
+          name="6"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="6"
+          disabled={isOperatorActive}
+        />
+        <button
+          aria-label="minus"
+          id="icon"
+          name="-"
+          onClick={(e) => props.onClick(e.currentTarget.name)}
+          className={OperatorClasses}
+          disabled={isSpanActive}
+          title="Minus"
+        >
+          <FontAwesomeIcon icon={faMinus} />
+        </button>
+      </div>
+
+      <div className="flex-row">
+        <input
+          name="1"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="1"
+          disabled={isOperatorActive}
+        />
+        <input
+          name="2"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="2"
+          disabled={isOperatorActive}
+        />
+        <input
+          name="3"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="3"
+          disabled={isOperatorActive}
+        />
+        <button
+          aria-label="plus"
+          id="icon"
+          name="+"
+          onClick={(e) => props.onClick(e.currentTarget.name)}
+          className={OperatorClasses}
+          disabled={isSpanActive}
+          title="Plus"
+        >
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </div>
+
+      <div className="flex-row">
+        <input
+          name="0"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="0"
+          disabled={isOperatorActive}
+        />
+        <button
+          aria-label="dot"
+          id="icon"
+          name="."
+          className="dot"
+          disabled={isOperatorActive}
+          title="Dot"
+          onClick={(e) => props.onClick(e.currentTarget.name)}
+        >
+          <FontAwesomeIcon icon={faCircle} />
+        </button>
+        {isSpanActive || isOperatorActive ? (
+          <button
+            aria-label="Ok"
+            type="button"
+            className={checkClasses}
+            onClick={props.toggleisActiveSpan}
+            title="Ok"
+          >
+            <FontAwesomeIcon icon={faCheck} />
+          </button>
+        ) : (
+          <input
+            name="="
+            onClick={(e) => props.onClick(e.target.name)}
+            type="button"
+            value="="
+            className="equal-key"
+            title="Equal"
+          />
+        )}
+      </div>
+
     </div>
   );
 };
--- src/components/Reusable/BasicKeypad.js
+++ src/components/Reusable/BasicKeypad.js
@@ -4,105 +4,114 @@ import { faBackspace, faCircle } from "@fortawesome/free-solid-svg-icons";
 
 const BasicKeypad = (props) => {
   const classes = props.class
-    ? `keypad-container basic-keypad-container temperature-keypad-btn`
-    : `keypad-container basic-keypad-container`;
+    ? `basic-keypad-container temperature-keypad-btn`
+    : `basic-keypad-container`;
   return (
     <div className={classes}>
-      <input
-        name="7"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="7"
-      />
-      <input
-        name="8"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="8"
-      />
-      <input
-        name="9"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="9"
-      />
-      <input
-        name="Ac"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="AC"
-        className="org-key"
-        id="allClear"
-      />
-      <input
-        name="4"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="4"
-      />
-      <input
-        name="5"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="5"
-      />
-      <input
-        name="6"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="6"
-      />
-      <input
-        name="1"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="1"
-      />
-      <input
-        name="2"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="2"
-      />
-      <input
-        name="3"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="3"
-      />
-      <input
-        name="0"
-        onClick={(e) => props.onClick(e.target.name)}
-        type="button"
-        value="0"
-      />
-      <button
-        aria-label="dot"
-        name="."
-        className="basic-dot"
-        onClick={(e) => props.onClick(e.currentTarget.name)}
-      >
-        <FontAwesomeIcon icon={faCircle} />
-      </button>
-
-      {props.isTemperature ? (
+      <div className="flex-row">
         <input
-          name="-"
-          id="tempPlusMinusbtn"
+          name="7"
           onClick={(e) => props.onClick(e.target.name)}
           type="button"
-          value="+/-"
+          value="7"
         />
-      ) : null}
-      <button
-        aria-label="backspace"
-        name="backspace"
-        onClick={(e) => props.onClick(e.currentTarget.name)}
-        className="org-key"
-        id="backspace"
-      >
-        <FontAwesomeIcon icon={faBackspace} />
-      </button>
+        <input
+          name="8"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="8"
+        />
+        <input
+          name="9"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="9"
+        />
+        <button
+          aria-label="backspace"
+          name="backspace"
+          onClick={(e) => props.onClick(e.currentTarget.name)}
+          className="org-key"
+          id="backspace"
+        >
+          <FontAwesomeIcon icon={faBackspace} />
+        </button>
+      </div>
+      <div className="flex-row">
+        {" "}
+        <input
+          name="4"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="4"
+        />
+        <input
+          name="5"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="5"
+        />
+        <input
+          name="6"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="6"
+        />
+        <input
+          name="Ac"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="AC"
+          className="org-key"
+          id="allClear"
+        />
+      </div>
+      <div className="flex-row">
+        <input
+          name="1"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="1"
+        />
+        <input
+          name="2"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="2"
+        />
+        <input
+          name="3"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="3"
+        />
+      </div>
+      <div className="flex-row">
+        <input
+          name="0"
+          onClick={(e) => props.onClick(e.target.name)}
+          type="button"
+          value="0"
+        />
+        <button
+          aria-label="dot"
+          name="."
+          className="basic-dot"
+          onClick={(e) => props.onClick(e.currentTarget.name)}
+        >
+          <FontAwesomeIcon icon={faCircle} />
+        </button>
+
+        {props.isTemperature ? (
+          <input
+            name="-"
+            id="tempPlusMinusbtn"
+            onClick={(e) => props.onClick(e.target.name)}
+            type="button"
+            value="+/-"
+          />
+        ) : null}
+      </div>
     </div>
   );
 };
