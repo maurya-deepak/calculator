bb5530d0525ef665e08650ec4a4a80472e5f82b2
maurya-deepak <dmaurya768@gmail.com>
2023-01-15 15:11:22 +0530

    Code refactor API move in apis folder Moved supported currencies list in constant file Create KEY object with special keys of keyboard Css changes

---
+++ src/constants/currency.js
@@ -0,0 +1,94 @@
+export const CURRENCY = [
+    {
+        name: "NewZealand Dollars NZD",
+        value: "NZD",
+        fullname: "NewZealand Dollars",
+    },
+    {
+        name: "Australian Dollars AUD",
+        value: "AUD",
+        fullname: "Australian Dollars",
+    },
+    { name: "Euro EUR", value: "EUR", fullname: "Euro" },
+    {
+        name: "Hong Kong Doller HKD",
+        value: "HKD",
+        fullname: "Hong Kong Doller",
+    },
+    {
+        name: "Canadian Dollar CAD",
+        value: "CAD",
+        fullname: "Canadian Dollar",
+    },
+    { name: "Japanese Yen JPY", value: "JPY", fullname: "Japanese Yen" },
+    { name: "Indian Rupee INR", value: "INR", fullname: "Indian Rupee" },
+    {
+        name: "Norwegian Krone NOK",
+        value: "NOK",
+        fullname: "Norwegian Krone",
+    },
+    { name: "Brazil BRL", value: "BRL", fullname: "Brazil" },
+    {
+        name: "Bulgarian Lev BGN",
+        value: "BGN",
+        fullname: "Bulgarian Lev",
+    },
+    { name: "China Yuan CNY", value: "CNY", fullname: "China Yuan" },
+    {
+        name: "Croatian Dinar HRK",
+        value: "HRK",
+        fullname: "Croatian Dinar",
+    },
+    { name: "Czech Koruna CZK", value: "CZK", fullname: "Czech Koruna" },
+    { name: "Danish Krone DKK", value: "DKK", fullname: "Danish Krone" },
+    {
+        name: "Indonesian Rupiah IDR",
+        value: "IDR",
+        fullname: "Indonesian Rupiah",
+    },
+    {
+        name: "Hungarian Forint HUF",
+        value: "HUF",
+        fullname: "Hungarian Forint",
+    },
+    {
+        name: "Icelandic Krona ISK",
+        value: "ISK",
+        fullname: "Icelandic Krona",
+    },
+    {
+        name: "Israel New Shekel ILS",
+        value: "ILS",
+        fullname: "Israel New Shekel",
+    },
+    { name: "Swiss Franc CHF", value: "CHF", fullname: "Swiss Franc" },
+    {
+        name: "Malaysian Ringgit MYR",
+        value: "MYR",
+        fullname: "Malaysian Ringgit",
+    },
+    { name: "Poland Zloty PLN", value: "PLN", fullname: "Poland Zloty" },
+    {
+        name: "Russian Ruble RUB",
+        value: "RUB",
+        fullname: "Russian Ruble",
+    },
+    {
+        name: "South Africa Rand ZAR",
+        value: "ZAR",
+        fullname: "South Africa Rand",
+    },
+    {
+        name: "Swedish Krona SEK",
+        value: "SEK",
+        fullname: "Swedish Krona",
+    },
+    { name: "Thai Baht THB", value: "THB", fullname: "Thai Baht" },
+    { name: "Turkish Lira TRY", value: "TRY", fullname: "Turkish Lira" },
+    {
+        name: "British Pound GBP",
+        value: "GBP",
+        fullname: "British Pound",
+    },
+    { name: "US Dollar USD", value: "USD", fullname: "US Dollar" },
+];
---
+++ src/constants/keyboard.js
@@ -0,0 +1,6 @@
+export const KEY = {
+    RESET: { Type: 'reset', Value: 'Ac' },
+    BACKSPACK: { Type: 'backspace', Value: 'backspace' },
+    DECIMAL: { Type: 'decimal', Value: '.' },
+    NUMBER: { Type: 'number' }
+}
